e32b45a3ea6489761ce4cdb9982eec8c6bddf650
Joho <joho@joho.io>
2017-01-03 16:20:15 -0500

    Add Ionicons font to build task

--- gulp/build.js
+++ gulp/build.js
@@ -52,6 +52,7 @@ gulp.task('html', ['inject', 'partials'], function () {
     // .pipe($.sourcemaps.init())
     .pipe($.replace('../../bower_components/bootstrap-sass/assets/fonts/bootstrap/', '../fonts/'))
     .pipe($.replace('../../bower_components/font-awesome/fonts', '../fonts'))
+    .pipe($.replace('../../bower_components/Ionicons/fonts', '../fonts'))
     .pipe($.cssnano())
     .pipe($.rev())
     // .pipe($.sourcemaps.write('maps'))
