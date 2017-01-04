c7bcb98530ab26f1e565333657914f952ac6820a
Joho <joho@joho.io>
2017-01-04 00:41:31 -0500

    Fixed font-awesome and Ionicons issue with gulp build and serve tasks

--- gulp/watch.js
+++ gulp/watch.js
@@ -10,7 +10,7 @@ function isOnlyChange(event) {
   return event.type === 'changed';
 }
 
-gulp.task('watch', ['markups', 'inject'], function () {
+gulp.task('watch', ['markups', 'inject', 'other'], function () {
 
   gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
 
